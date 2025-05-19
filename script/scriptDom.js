87f94535cc9560abc5ff7bd9de47746411be8183
sabbato-dev <sabbato.bruno@gmail.com>
2025-05-19 17:23:31 -0300

    exercicio DOM

---
+++ script/scriptDom.js
@@ -0,0 +1,100 @@
+function calcularMedia() {
+    
+    
+    const nomeAluno = document.getElementById("nome").value;
+    const nota1 = parseFloat(document.getElementById("nota1").value);
+    const nota2 = parseFloat(document.getElementById("nota2").value);
+    const nota3 = parseFloat(document.getElementById("nota3").value);
+    const nota4 = parseFloat(document.getElementById("nota4").value);
+    
+    let media= (nota1 + nota2 + nota3 + nota4) / 4;
+    document.getElementById("mediaAluno").value = media;
+    
+    if (media >= 7) {
+        document.getElementById("resultado").value = "Aprovado";
+    } else if (media >= 5) {
+        document.getElementById("resultado").value = "Recuperacao";
+    } else {
+        document.getElementById("resultado").value = "Reprovado";
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function main() {
+//     let notabim1, notabim2, notabim3, notabim4, notabimsomada, mediafinal, notabimrecuperacao; //declarando variaveis em JS
+ 
+//     notabim1=0; //atribuindo variável em JS
+//     let numero = Number("2"); //declarando
+//     notabim1 = Number(prompt('Entre com o valor da primeira nota'));
+//     notabim2 = Number(prompt('Entre com o valor da segunda nota'));
+//     notabim3 = Number(prompt('Entre com o valor da terceira nota'));
+//     notabim4 = Number(prompt('Entre com o valor da quarta nota'));
+//     notabimsomada =(notabim1 + notabim2 + notabim3 + notabim4);
+//     mediafinal = notabimsomada / 4;
+//     alert("A media final é");
+//     alert(mediafinal);
+ 
+//     if (mediafinal >= 7) {
+//         alert("Aprovado");
+       
+//     } else {
+//         if (mediafinal >= 5) {
+//             alert("O aluno está de recuperação");
+//             alert("Digite a nota da recuperação");
+//             notabimrecuperacao = Number(prompt('Enter a value for nota de recuperacao'));
+//             if (notabimrecuperacao >= 7) {
+//                 alert("Aprovado");
+//             } else {
+//                 alert("Reprovado");
+//             }
+//         } else {
+//             alert("Reprovado");
+//         }
+//     }
+// }
+// main();
